95b1774536cc403c11626bbce70a4c8e0f479fee
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-03 04:10:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,25 +3,4 @@ class Dog
     @name = name
     @breed = breed
   end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name 
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end 
-  
-  def bread 
-    @bread 
-  end
-
 end
